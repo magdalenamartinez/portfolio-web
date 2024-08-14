d679df142300011335e7eb23d4b102941cc31fe8
magdalenamartinez <magdalena.martinezsanchez01@gmail.com>
2024-08-14 12:18:54 +0200

    gitpages

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import './App.css';
-import './css/importcss.js';
 import Header from './components/header';
 import Footer from './components/footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Index from './components/index.js';
-import Contact from './components/contact.js';
+import Index from './components/index';
+import Contact from './components/contact';
 import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n/i18n.js';
-
+import i18n from './i18n/i18n';
 
 function App() {
   return (
-    <I18nextProvider>
-    <div className="generalClass">
+    <I18nextProvider i18n={i18n}>
       <Router>
-          <Header/>
-          <Routes>
-            <Route path='/' element={<Index/>}></Route>
-            <Route path='/contact' element={<Contact/>}></Route>
-          </Routes>
-          <Footer/>
+        <Header/>
+        <Routes>
+          <Route path='https://magdalenamartinez.github.io/portfolio-web/' element={<Index/>} />
+          <Route path='https://magdalenamartinez.github.io/portfolio-web/contact' element={<Contact/>} />
+        </Routes>
+        <Footer/>
       </Router>
-    </div> 
     </I18nextProvider>
-   
-    
   );
 }
 
