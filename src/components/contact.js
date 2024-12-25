405a39a5547be02598b342bae70953bf8e2f71fa
magdalenamartinez <magdalena.martinezsanchez01@gmail.com>
2024-12-25 09:52:12 +0100

    tryhackme

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,7 +3,7 @@ import './../App.css';
 import './../css/importcss.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faTryHackMe } from '@fortawesome/free-brands-svg-icons';
 import mePhoto from './../assets/me.jpg';
 import { Reveal } from './reveal.tsx';
 
@@ -25,6 +25,9 @@ const Contact = () => {
               </h2>
               <h2 className="Text_1_5 paddingLeft"><FontAwesomeIcon icon={faLinkedin} className='iconClass_1'/>
                 <a href="https://www.linkedin.com/in/magdalena-martinez-sanchez" target="_blank" rel="noopener noreferrer" className="linkClass2">www.linkedin.com/in/magdalena-martinez-sanchez</a>
+              </h2>
+              <h2 className="Text_1_5 paddingLeft"><FontAwesomeIcon icon={faTryHackMe} className='iconClass_1'/>
+                <iframe src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=2032396" style='border:none;'></iframe>
               </h2>
             </div>
           </div>
