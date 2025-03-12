0e841cf475a73470739020c8fd3a010bc86370e5
magdalenamartinez <magdalena.martinezsanchez01@gmail.com>
2025-03-12 18:40:10 +0100

    cv

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClipboard } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faClipboard } from '@fortawesome/free-solid-svg-icons';
 import curriculum from './../assets/CV_EN.pdf';
 import { useTranslation } from 'react-i18next';
 import spanishIco from '../assets/spain.png';
 import  englishIco from '../assets/uk.png';
 import frenchIco from '../assets/france.png';
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
     const { i18n, t } = useTranslation();
 
     const changeLanguage = (lng) => {
@@ -28,15 +29,20 @@ const Header = () => {
             <button onClick={() => changeLanguage('es')} className='buttonLanguage'><img src={spanishIco} alt={"ES"} className="languageClass"/></button>
             <button onClick={() => changeLanguage('fr')} className='buttonLanguage'><img src={frenchIco} alt={"FR"} className="languageClass"/></button>
         </div>
-      
-        <nav className='flex_1'>
-            <ul className='ulClass'>
+        <div className='flex_1'>
+            <button className="menuButton" onClick={() => setIsOpen(!isOpen)}>
+                <FontAwesomeIcon icon={faBars} style={{"color":"white"}} className='iconClass'/>
+            </button>
+        </div>
+        <nav className={`flex_1 ${isOpen ? "open" : ""}`}>
+            <ul className={`menuList ${isOpen ? "open" : ""} ulClass`}>
                 <li className='liClass'><Link className="linkClass" to="/">{t('home')}</Link></li>
                 <li className='liClass'><a className="linkClass" href={curriculum} download="curriculum.pdf">{t('resume')}</a></li>
                 <li className='liClass'><a className="linkClass" href="https://github.com/magdalenamartinez">{t('projects')}</a></li>
                 <li className='liClass'><Link className="linkClass" to="/contact">{t('contact')}</Link></li>
             </ul>
         </nav>
+      
     </div>
     );
 }
